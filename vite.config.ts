ef46f9c7c3386fa5daf36b8dba99bea9bd85a58a
mineot <mineot@gmail.com>
2025-02-25 20:10:32 -0300

    master - create a base structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      themes: path.resolve(__dirname, 'src/themes'),
-      mixins: path.resolve(__dirname, 'src/mixins'),
+      '@': path.resolve(__dirname, 'src'),
+      '~': path.resolve(__dirname, 'src/styles'),
+      '~mixin': path.resolve(__dirname, 'src/styles/mixins'),
+      '~effect': path.resolve(__dirname, 'src/styles/mixins/effects'),
+      '~element': path.resolve(__dirname, 'src/styles/mixins/elements'),
+      '~util': path.resolve(__dirname, 'src/styles/mixins/utils'),
     },
   },
 });
