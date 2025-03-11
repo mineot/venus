295f9391b67ddcd438ded59ec80b33bd459331c9
mineot <mineot@gmail.com>
2025-03-11 20:58:50 -0300

    tailwind - clear project

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '~': path.resolve(__dirname, 'src/styles'),
-      '~effect': path.resolve(__dirname, 'src/styles/effects'),
-      '~element': path.resolve(__dirname, 'src/styles/elements'),
-      '~function': path.resolve(__dirname, 'src/styles/functions'),
-      '~tag': path.resolve(__dirname, 'src/styles/tags'),
-      '~util': path.resolve(__dirname, 'src/styles/utils'),
       modules: path.resolve(__dirname, 'node_modules'),
     },
   },
